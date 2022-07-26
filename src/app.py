baebd00e1ad09145b6c544d48cce979f9cb4741e
jg <57115019+joeyagreco@users.noreply.github.com>
2022-07-26 16:44:24 -0500

    initial commit

---
+++ src/app.py
@@ -0,0 +1,19 @@
+import os
+
+from flask import Flask
+
+from server.util.EnvironmentReader import EnvironmentReader
+
+# set directories
+
+templateDir = os.path.join(os.path.dirname(__file__), EnvironmentReader.get("TEMPLATE_DIR_RELATIVE_PATH"))
+staticDir = os.path.join(os.path.dirname(__file__), EnvironmentReader.get("STATIC_DIR_RELATIVE_PATH"))
+app = Flask(__name__, template_folder=templateDir, static_folder=staticDir)
+
+if __name__ == "__main__":
+    from server.controller.indexController import *  # noqa
+
+    if EnvironmentReader.get("TEST_ENVIRONMENT") == "True":
+        app.run(debug=True)
+    else:
+        app.run(host="0.0.0.0", port=80)
