0474f9d1ce37a38990c3d553fd2674aadff1a9dd
jg <57115019+joeyagreco@users.noreply.github.com>
2022-07-26 18:58:15 -0500

    working on index

---
+++ src/client/static/js/general.js
@@ -0,0 +1,40 @@
+// for general purpose js methods
+
+document.onreadystatechange = function () {
+    if (document.readyState !== "complete") {
+        startLoading();
+    } else {
+        // stopLoading();
+    }
+}
+
+function redirect(path) {
+    startLoading();
+    window.location = path;
+}
+
+// method for sending POST requests
+window.post = function (url, data) {
+    return fetch(url, {method: "POST", body: JSON.stringify(data)});
+}
+
+// method for sending PUT requests
+window.put = function (url, data) {
+    return fetch(url, {method: "PUT", body: JSON.stringify(data)});
+}
+
+// method for sending DELETE requests
+window.del = function (url) {
+    return fetch(url, {method: "DELETE"});
+}
+
+function startLoading() {
+    document.querySelector("#loader-wrapper").style.display = "flex";
+    document.querySelector("#loader-wrapper").style.visibility = "visible";
+}
+
+function stopLoading() {
+    document.querySelector("#loader-wrapper").style.display = "none";
+    document.querySelector("#loader-wrapper").style.visibility = "hidden";
+    document.querySelector("body").style.visibility = "visible";
+}
