486f66a96ef796d1203a7567ad9a05b3a5f26646
jg <57115019+joeyagreco@users.noreply.github.com>
2022-07-27 18:04:30 -0500

    working on index page

--- src/client/static/js/index.js
+++ src/client/static/js/index.js
@@ -14,7 +14,7 @@ function onDownloadStatsButtonPress() {
     });
 }
 
-function validateYearsInput() {
+function fixYearsInput() {
     const yearsInputElement = document.getElementById("years");
     const allowedCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ","]
     let validatedYearInputText = ""
@@ -25,4 +25,46 @@ function validateYearsInput() {
         }
     }
     yearsInputElement.value = validatedYearInputText;
+    checkIfValidInput();
+}
+
+function isValidYearsInput(yearsStr) {
+    if (yearsStr === "") {
+        return false;
+    }
+    let years = yearsStr.split(",");
+    console.log(years);
+    for (let i = 0; i < years.length; i++) {
+        const year = years[i]
+        if (year === "") {
+            continue;
+        }
+        if (year.length !== 4 || isNaN(year) || parseInt(year) < 1920 || parseInt(year) > 2200) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function checkIfValidInput() {
+    // does basic validation on league info and enables/disables download button accordingly
+    const leagueId = document.getElementById("league-id").value;
+    const years = document.getElementById("years").value;
+    if (leagueId === "" || !isValidYearsInput(years)) {
+        disableDownloadButton();
+    } else {
+        enableDownloadButton();
+    }
+}
+
+function disableDownloadButton() {
+    const downloadButton = document.getElementById("download-stats-button");
+    downloadButton.classList.add("disabled")
+    downloadButton.disabled = true;
+}
+
+function enableDownloadButton() {
+    const downloadButton = document.getElementById("download-stats-button");
+    downloadButton.classList.remove("disabled")
+    downloadButton.disabled = false;
 }
