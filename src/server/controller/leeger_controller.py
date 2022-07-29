520ab78fd6b5b8f7cc285fffa6dee07410145afb
jg <57115019+joeyagreco@users.noreply.github.com>
2022-07-29 14:22:42 -0500

    cleanup

--- src/server/controller/leeger_controller.py
+++ src/server/controller/leeger_controller.py
@@ -3,7 +3,6 @@ import os
 from app import app
 from flask import request, redirect, url_for
 
-from src.server.exception.BadRequestException import BadRequestException
 from src.server.model.LeegerRequest import LeegerRequest
 from src.server.service.LeagueService import LeagueService
 from src.server.util.ControllerHelper import ControllerHelper
@@ -17,6 +16,7 @@ def leeger():
         leeger_request = LeegerRequest.from_json(data)
         directory_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "file_store"))
         LeagueService.save_excel_stats(leeger_request, directory_path)
-    except BadRequestException as e:
+    except Exception as e:
+        # TODO: clean this up and parse certain exceptions for cleaner display in UI
         error_message = str(e)
     return redirect(url_for("index", error_message=error_message))
